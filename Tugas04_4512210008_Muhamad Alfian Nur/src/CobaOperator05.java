c7d6cc66452c8334abe344e527af5cac9996231b
manbensam <abdulrahmanpancasila@gmail.com>
2014-04-15 14:08:40 +0700

    Pemrog01RegulerGenap1314

---
+++ Tugas04_4512210008_Muhamad Alfian Nur/src/CobaOperator05.java
@@ -0,0 +1,18 @@
+//Ceated By Muhamad Alfian Nur
+//Update : 14 April 2014
+
+//perintah operator
+
+package Latihan01.sesi1.bin;
+
+public class CobaOperator05
+{
+    public static void main (String[] Alfian)
+	{
+	    
+		System.out.println("Nilai Binary		[bin] 31= " +Integer.parseInt("31",2));
+		System.out.println("Nilai Octal			[Oct] 31= " +Byte.parseByte("31",8));
+		System.out.println("Nilai Decimal		[Dec] 31= " +Short.parseShort("31",10));
+		System.out.println("Nilai Hexadicimal	[Hex] 31= " +Short.parseShort("31",16));
+	}
+}
---
+++ Tugas04_4512210008_Muhamad Alfian Nur/src/increment.java
@@ -0,0 +1,38 @@
+//Ceated By Muhamad Alfian Nur
+//Update : 14 April 2014
+
+/**
+* class increment
+* author (Alfian)
+* version (op-02)
+*/
+
+package Latihan01.sesi2.bin;
+public class increment
+{
+    public static void main(String[] Alfian)
+	{
+	    int a;
+		a=120;
+		System.out.println ("contoh postfix increment :");
+		System.out.println (a);
+		System.out.println (a++);
+		System.out.println (a);
+		a=120;
+		System.out.println ("contoh prefix increment :");
+		System.out.println (a);
+		System.out.println (++a);
+		System.out.println (a);
+		a=120;
+		System.out.println ("contoh postfix decrement :");
+		System.out.println (a);
+		System.out.println (a--);
+		System.out.println (a);
+		a=120;
+		System.out.println ("contoh prefix decrement :");
+		System.out.println (a);
+		System.out.println (--a);
+		System.out.println (a);
+	}
+}	
+		
